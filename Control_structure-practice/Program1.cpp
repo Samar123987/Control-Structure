4336f3d21d8991e5de05ca385a8c3facc2d637c8
Samar123987 <samarbabar02@gmail.com>
2025-02-06 00:02:41 +0500

    C++

--- Control_structure-practice/Program1.cpp
+++ Control_structure-practice/Program1.cpp
@@ -1,1 +1,21 @@
+//	Write a program to check if a number entered by the user is positive, negative, or zero using if-else.
+
+ #include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num > 0)
+        cout << "The number is positive.";
+    else if (num < 0)
+        cout << "The number is negative.";
+    else
+        cout << "The number is zero.";
+
+    return 0;
+}
+
 
