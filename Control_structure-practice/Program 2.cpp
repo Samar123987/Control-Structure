366181915fc640a450542cade03ca00cbbfffde0
Samar123987 <samarbabar02@gmail.com>
2025-02-06 07:03:13 +0500

    C++

--- Control_structure-practice/Program 2.cpp
+++ Control_structure-practice/Program 2.cpp
@@ -1,1 +1,18 @@
+//Write a program that takes a number as input and checks whether it is even or odd using if-
+else.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+        cout << "Enter a number: ";
+            cin >> num;
+
+                if (num % 2 == 0)
+                        cout << "The number is even.";
+                            else
+                                    cout << "The number is odd.";
+
+                                        return 0;
+                                        }
 
