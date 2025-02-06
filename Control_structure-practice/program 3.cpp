214e4fd23f9d7bd9c074bb64a10839cd96d5914b
Samar123987 <samarbabar02@gmail.com>
2025-02-06 07:04:06 +0500

    C++

--- Control_structure-practice/program 3.cpp
+++ Control_structure-practice/program 3.cpp
@@ -1,1 +1,20 @@
+// Write a program to find the largest of three numbers using if-else.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+        cout << "Enter three numbers: ";
+            cin >> a >> b >> c;
+
+                if (a > b && a > c)
+                        cout << "Largest number is: " << a;
+                            else if (b >a && b > c)
+                                    cout << "Largest number is: " << b;
+                                        else
+                                                cout << "Largest number is: " << c;
+
+                                                    return 0;
+                                                    }
 
