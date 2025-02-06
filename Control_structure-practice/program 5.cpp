43b5fb2ef4e9b6677e79d7e20f283fcf51919169
Samar123987 <samarbabar02@gmail.com>
2025-02-06 07:06:08 +0500

    C++

--- Control_structure-practice/program 5.cpp
+++ Control_structure-practice/program 5.cpp
@@ -1,1 +1,17 @@
+// Write a program that checks whether a year entered by the user is a leap year or not using if-else.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int year;
+        cout << "Enter a year: ";
+            cin >> year;
+
+                if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+                        cout << year << " is a leap year.";
+                            else
+                                    cout << year << " is not a leap year.";
+
+                                        return 0;
+                                        }
 
